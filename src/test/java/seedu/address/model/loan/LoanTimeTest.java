7b0bc9fc22678d6e039a50426cdf8e132ac7604a
prokarius <rollingwithoutslipping@yahoo.com>
2018-10-07 17:23:04 +0800

    Add validation tests for LoanTime class

--- src/test/java/seedu/address/model/loan/LoanTimeTest.java
+++ src/test/java/seedu/address/model/loan/LoanTimeTest.java
@@ -3,8 +3,9 @@ package seedu.address.model.loan;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-// TODO: Add tests
 public class LoanTimeTest {
 
     @Test
@@ -18,8 +19,7 @@ public class LoanTimeTest {
 
     // Note that this method will not return equals as it is time dependent
     // Tried and Tested though. It works.
-    @Test
-    public void constructorInputCheckCurrentDate(){
+    public void constructorInputCheckCurrentDate() {
         LoanTime loanTime = new LoanTime("00:25");
 
         // Replace with your current date to test
@@ -28,10 +28,59 @@ public class LoanTimeTest {
 
     // Note that this method will not return equals as it is time dependent
     // Tried and Tested though. It works.
-    public void constructorInputCheckCurrentTime(){
+    public void constructorInputCheckCurrentTime() {
         LoanTime loanTime = new LoanTime();
 
         // Replace with your current time and date to test
         assertEquals("2018-10-07, 16:38", loanTime.toString());
+    }
+
+    @Test
+    public void isValidLongLoanTimeTests() {
+        assertFalse(LoanTime.isValidLongLoanTimeFormat("")); // Empty String
+        assertFalse(LoanTime.isValidLongLoanTimeFormat("      ")); // Spaces only
+
+        assertFalse(LoanTime.isValidLongLoanTimeFormat("10-12-25 14:09")); // Invalid Year
+        assertFalse(LoanTime.isValidLongLoanTimeFormat("2010-May-25 14:09")); // Invalid Month
+        assertFalse(LoanTime.isValidLongLoanTimeFormat("2010-12-25 14-09")); // Incorrect separator format
+
+        assertTrue(LoanTime.isValidLongLoanTimeFormat("2010-12-25 04:09")); // An example of a correct one
+        assertTrue(LoanTime.isValidLongLoanTimeFormat("2010-12-25     14:09")); // Multiple Spaces
+    }
+
+    @Test
+    public void isValidShortLoanTimeTests() {
+        assertFalse(LoanTime.isValidShortLoanTimeFormat("")); // Empty String
+        assertFalse(LoanTime.isValidShortLoanTimeFormat("      ")); // Spaces only
+
+        assertFalse(LoanTime.isValidShortLoanTimeFormat("14-09")); // Incorrect separator format
+        assertFalse(LoanTime.isValidShortLoanTimeFormat("1:09")); // Incorrect hour format
+        assertFalse(LoanTime.isValidShortLoanTimeFormat("11:09 PM")); // Incorrect time format
+
+        assertTrue(LoanTime.isValidShortLoanTimeFormat("04:55")); // An example of a correct one
+    }
+
+    @Test
+    // Note that input params are already in the format YYYY-MM-DD
+    public void isValidDateTests() {
+        assertFalse(LoanTime.isValidDate("2011-13-16")); // 13th Month
+        assertFalse(LoanTime.isValidDate("2011-01-32")); // 32th Day
+
+        assertTrue(LoanTime.isValidDate("2011-01-31")); // January has 31 days
+        assertFalse(LoanTime.isValidDate("2011-06-31")); // June only has 30 days
+
+        assertFalse(LoanTime.isValidDate("2014-02-29")); // Not a leap years
+        assertTrue(LoanTime.isValidDate("2012-02-29")); // Is a leap years
+
+        assertTrue(LoanTime.isValidDate("2012-05-29")); // A correct example
+    }
+
+    @Test
+    public void isValidTimeTests() {
+        assertFalse(LoanTime.isValidTime("26:16")); // Too many hours
+        assertFalse(LoanTime.isValidTime("21:67")); // Too many minutes
+
+        assertTrue(LoanTime.isValidTime("21:27")); // A correct example
+        assertTrue(LoanTime.isValidTime("00:00")); // Midnight
     }
 }
