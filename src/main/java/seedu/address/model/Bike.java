ea6439880392d059ad9df80a73f7228a73a091bf
pm451 <43968500+pm451@users.noreply.github.com>
2018-10-12 22:09:17 +0200

    Create Bike.java

---
+++ src/main/java/seedu/address/model/Bike.java
@@ -0,0 +1,19 @@
+package seedu.address.model;
+
+public class Bike {
+
+    public String name;
+
+    public Bike(String name) {
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
