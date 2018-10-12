f335e9ef986943091db6cf1ed19113867f6fb3ce
pm451 <43968500+pm451@users.noreply.github.com>
2018-10-12 22:23:49 +0200

    Update Bike.java

--- src/main/java/seedu/address/model/Bike.java
+++ src/main/java/seedu/address/model/Bike.java
@@ -1,11 +1,14 @@
 package seedu.address.model;
 
+/**
+ * Represents a Bike in the address book.
+ */
 public class Bike {
 
-    public String name;
+   private String name;
 
     public Bike(String name) {
-        this.name=name;
+        this.name = name;
     }
 
     public String getName() {
