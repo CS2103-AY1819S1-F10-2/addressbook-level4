7a455b8cd32ae9dd3ef6f4ef1caf5e1358332bb0
FongYuan <42345573+FongYuan@users.noreply.github.com>
2018-10-22 03:21:26 +0800

    Correct checkstyles

--- src/test/java/seedu/address/testutil/TypicalLoanBook.java
+++ src/test/java/seedu/address/testutil/TypicalLoanBook.java
@@ -1,9 +1,12 @@
 package seedu.address.testutil;
 
-import seedu.address.model.bike.Bike;
 import seedu.address.model.LoanBook;
+import seedu.address.model.bike.Bike;
 import seedu.address.model.loan.Loan;
 
+/**
+ * A utility class containing a factory for a {@code LoanBook} object to be used in tests.
+ */
 public class TypicalLoanBook {
     /**
      * Returns an {@code LoanBook} with all the typical bikes and loans.
