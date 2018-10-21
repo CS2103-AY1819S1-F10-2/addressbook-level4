e0ee1c0f12ccb4497fba9e00cd4bc38634c0ac89
FongYuan <42345573+FongYuan@users.noreply.github.com>
2018-10-22 02:59:47 +0800

    Add typical bikes to TypicalLoanBook

--- src/test/java/seedu/address/testutil/TypicalLoanBook.java
+++ src/test/java/seedu/address/testutil/TypicalLoanBook.java
@@ -1,17 +1,21 @@
 package seedu.address.testutil;
 
+import seedu.address.model.bike.Bike;
 import seedu.address.model.LoanBook;
 import seedu.address.model.loan.Loan;
 
 public class TypicalLoanBook {
     /**
-     * Returns an {@code LoanBook} with all the typical loans.
+     * Returns an {@code LoanBook} with all the typical bikes and loans.
      */
     public static LoanBook getTypicalLoanBook() {
-        LoanBook ab = new LoanBook();
-        for (Loan loan : TypicalLoans.getTypicalLoans()) {
-            ab.addLoan(loan);
+        LoanBook lb = new LoanBook();
+        for (Bike bike : TypicalBikes.getTypicalBikes()) {
+            lb.addBike(bike);
         }
-        return ab;
+        for (Loan loan : TypicalLoans.getTypicalLoans()) {
+            lb.addLoan(loan);
+        }
+        return lb;
     }
 }
