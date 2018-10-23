57ade3702cd6949c0c9e377461569dc7073d9a12
FongYuan <42345573+FongYuan@users.noreply.github.com>
2018-10-23 18:26:22 +0800

    Remove trailing whitespace

--- src/main/java/seedu/address/model/UniqueListItem.java
+++ src/main/java/seedu/address/model/UniqueListItem.java
@@ -10,7 +10,7 @@ public interface UniqueListItem<T extends UniqueListItem<T>> {
      * Checks if both items are weakly identical. A UniqueList should not have two weakly identical items.
      * "Weakly identical" is opposed to {@code equals()} because we may not want items to be exactly the same
      * to be considered unique.
-     * 
+     *
      * @param other The other item to compare against this item.
      * @return true iff both items are weakly identical.
      */
