72370a620b5f0f5b847f95cb2696c3ca2e17f0cd
wn96 <weineng.a@gmail.com>
2018-10-23 12:29:55 +0800

    Improve test coverage

--- src/test/java/seedu/address/logic/parser/SetPasswordCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SetPasswordCommandParserTest.java
@@ -1,4 +1,19 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_LOAN;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.SetPasswordCommand;
+import seedu.address.model.Password;
+
 public class SetPasswordCommandParserTest {
+
+    private SetPasswordCommandParser parser = new SetPasswordCommandParser("a12345 newpass1");
+
+    @Test
+    public void parse_validArgs_returnsSetPasswordCommand() {
+        assertParseSuccess(parser, "1", new SetPasswordCommand(INDEX_FIRST_LOAN));
+    }
 }
