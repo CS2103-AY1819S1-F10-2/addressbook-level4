c3ea20dd51d3e8b72c5628cbeb997288fade5e2c
FongYuan <42345573+FongYuan@users.noreply.github.com>
2018-10-28 18:25:25 +0800

    Add BikeCard class and fxml

---
+++ src/main/java/loanbook/ui/BikeCard.java
@@ -0,0 +1,18 @@
+package loanbook.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import loanbook.model.bike.Bike;
+
+public class BikeCard extends ListCard<Bike> {
+
+    private static final String FXML = "BikeCard.fxml";
+
+    @FXML
+    private Label name;
+
+    public BikeCard(Bike bike, int displayedIndex) {
+        super(FXML, bike, displayedIndex);
+        name.setText(bike.getName().value);
+    }
+}
